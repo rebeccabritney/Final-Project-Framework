0fe392b0dc13e061d6d4d8b2f06ec67a0ba871f9
rebeccabritney <rebeccabritney25@gmail.com>
2020-05-08 18:09:33 +0700

    test

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import { Cards, CountryPicker, Chart } from './components';
+import { fetchData } from './api/';
+import styles from './App.module.css';
+import image from './images/image.png';
+
+class App extends React.Component {
+  state = {
+    data: {},
+    dataCountry: {},
+    country: '',
+  }
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+
+    this.setState({ data: fetchedData });
+  }
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+
+    this.setState({ dataCountry: fetchedData, country: country });
+  }
+
+  render() {
+    const { data, country } = this.state;
+    return (
+      <div className={styles.container}>
+        <img className={styles.image} src={image} alt="COVID-19" />
+        <Cards data={this.state.data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        {country && <Cards data={this.state.dataCountry} country={country} />}
+        <Chart data={data} country={country} />
+      </div>
+    );
+  }
+}
+
+export default App;
