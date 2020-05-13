c7c15e3f6bc3bf1523e53755d9ad697112f13689
rebeccabritney <rebeccabritney25@gmail.com>
2020-05-13 19:21:59 +0700

    new commit

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Card, CardContent, Typography, Grid } from "@material-ui/core";
 import CountUp from "react-countup";
 import cx from "classnames";
+//import { makeStyles } from '@material-ui/core/styles';
 
 import styles from "./Cards.module.css";
 
@@ -24,8 +25,8 @@ const Info = ({
           component={Card}
           className={cx(styles.card, styles.infected)}
         >
-          <CardContent>
-            <Typography color="textSecondary" gutterBottom>
+          <CardContent align="center">
+            <Typography color="secondary" gutterBottom>
               Infected
             </Typography>
             <Typography variant="h5" component="h2">
@@ -51,8 +52,8 @@ const Info = ({
           component={Card}
           className={cx(styles.card, styles.recovered)}
         >
-          <CardContent>
-            <Typography color="textSecondary" gutterBottom>
+          <CardContent align="center">
+            <Typography color="secondary" gutterBottom>
               Recovered
             </Typography>
             <Typography variant="h5" component="h2">
@@ -78,8 +79,8 @@ const Info = ({
           component={Card}
           className={cx(styles.card, styles.deaths)}
         >
-          <CardContent>
-            <Typography color="textSecondary" gutterBottom>
+          <CardContent align="center">
+            <Typography color="secondary" gutterBottom>
               Deaths
             </Typography>
             <Typography variant="h5" component="h2">
@@ -102,4 +103,75 @@ const Info = ({
     </div>
   );
 };
+
+
+// import React from 'react';
+// import { makeStyles } from '@material-ui/core/styles';
+// import Grid from '@material-ui/core/Grid';
+// import Paper from '@material-ui/core/Paper';
+// import Typography from '@material-ui/core/Typography';
+// import ButtonBase from '@material-ui/core/ButtonBase';
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     flexGrow: 1,
+//   },
+//   paper: {
+//     padding: theme.spacing(2),
+//     margin: 'auto',
+//     maxWidth: 500,
+//   },
+//   image: {
+//     width: 128,
+//     height: 128,
+//   },
+//   img: {
+//     margin: 'auto',
+//     display: 'block',
+//     maxWidth: '100%',
+//     maxHeight: '100%',
+//   },
+// }));
+
+// export default function ComplexGrid() {
+//   const classes = useStyles();
+
+//   return (
+//     <div className={classes.root}>
+//       <Paper className={classes.paper}>
+//         <Grid container spacing={2}>
+//           <Grid item>
+//             <ButtonBase className={classes.image}>
+//               <img className={classes.img} alt="complex" src="/static/images/grid/complex.jpg" />
+//             </ButtonBase>
+//           </Grid>
+//           <Grid item xs={12} sm container>
+//             <Grid item xs container direction="column" spacing={2}>
+//               <Grid item xs>
+//                 <Typography gutterBottom variant="subtitle1">
+//                   Standard license
+//                 </Typography>
+//                 <Typography variant="body2" gutterBottom>
+//                   Full resolution 1920x1080 • JPEG
+//                 </Typography>
+//                 <Typography variant="body2" color="textSecondary">
+//                   ID: 1030114
+//                 </Typography>
+//               </Grid>
+//               <Grid item>
+//                 <Typography variant="body2" style={{ cursor: 'pointer' }}>
+//                   Remove
+//                 </Typography>
+//               </Grid>
+//             </Grid>
+//             <Grid item>
+//               <Typography variant="subtitle1">$19.00</Typography>
+//             </Grid>
+//           </Grid>
+//         </Grid>
+//       </Paper>
+//     </div>
+//   );
+// }
+
 export default Info;
